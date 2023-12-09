136597597e818bced709265c6eedf0c312e0907d
B-e-d-B-o-y <IgorPerminov2004@mail.ru>
2023-12-10 01:19:19 +1000

    Создал базу данных со статьями и синхронизировал её с сайтом

--- www/blocks/head.php
+++ www/blocks/head.php
@@ -1,3 +1,7 @@
+<?php 
+    require_once "functions/functions.php"
+?>
+
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/stile.css">
